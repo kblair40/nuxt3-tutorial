ba7ba594b599cd8090b8ef0f76d33b7960263116
Kevin Blair <kblair40@gmail.com>
2023-07-19 06:07:53 -0500

    Finish id page

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const key = 'apikey=ff0d50dd'
+const url = `http://www.omdbapi.com/?`
+
+
+const fullUrl = url + `i=${route.params.id}&${key}`;
+
+// Using useAsyncData prevents duplicate server/client fetches
+const { data } = useAsyncData(() => {
+    return $fetch(fullUrl)
+})
+
+</script>
+
 <template>
     <div>
-        <h1>{{ $route.params.id }}</h1>
+        <pre>
+            {{ data }}
+        </pre>
     </div>
 </template>
